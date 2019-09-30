d0861183788dd7d1ff88424a81b98d0ebf275387
borisgrafx <boralbel@mail.ru>
2019-09-30 19:24:09 +0300

    contain full lesson 2 (i am maked itttt) and some improvements in lesson 3

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -3,6 +3,9 @@
 package lesson2.task2
 
 import lesson1.task1.sqr
+import kotlin.math.abs
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 /**
  * Пример
@@ -29,7 +32,8 @@ fun isNumberHappy(number: Int): Boolean =
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
+    (abs(x1 - x2) == abs(y1 - y2) || (x1 == x2) || (y1 == y2))
 
 
 /**
@@ -56,7 +60,7 @@ fun daysInMonth(month: Int, year: Int): Int = when {
 fun circleInside(
     x1: Double, y1: Double, r1: Double,
     x2: Double, y2: Double, r2: Double
-): Boolean = TODO()
+): Boolean = sqrt((x2 - x1).pow(2) + (y2 - y1).pow(2)) + r1 <= r2
 
 /**
  * Средняя
--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -6,6 +6,7 @@ import kotlin.math.sqrt
 import kotlin.math.max
 import kotlin.math.pow
 import kotlin.math.abs
+import kotlin.math.PI
 
 /**
  * Пример
@@ -71,8 +72,8 @@ fun digitCountInNumber(n: Int, m: Int): Int =
  * Использовать операции со строками в этой задаче запрещается.
  */
 fun digitNumber(n: Int): Int {
-    var l: Int = abs(n)
-    var k: Int = 1
+    var l = abs(n)
+    var k = 1
     while (l > 9) {
         l /= 10
         k += 1
@@ -87,6 +88,7 @@ fun digitNumber(n: Int): Int {
  * Ряд Фибоначчи определён следующим образом: fib(1) = 1, fib(2) = 1, fib(n+2) = fib(n) + fib(n+1)
  */
 
+//Тут пока долго делается. До октябрьской проверки попробую придумать метод оптимизации.
 fun fib(n: Int): Int = if (n < 3) 1 else (fib(n - 2) + fib(n - 1))
 
 
@@ -97,10 +99,17 @@ fun fib(n: Int): Int = if (n < 3) 1 else (fib(n - 2) + fib(n - 1))
  * минимальное число k, которое делится и на m и на n без остатка
  */
 fun lcm(m: Int, n: Int): Int {
-    var k: Int = m * n
-    for (i in max(m, n)..m * n) if ((i % m == 0) && (i % n == 0) && (i < k)) k = i
-    return k
+    var m1 = m
+    var n1 = n
+    while (m1 != n1) {
+        if (m1 > n1)
+            m1 -= n1
+        else
+            n1 -= m1
+    }
+    return m * n / m1
 }
+
 
 /**
  * Простая
@@ -108,9 +117,11 @@ fun lcm(m: Int, n: Int): Int {
  * Для заданного числа n > 1 найти минимальный делитель, превышающий 1
  */
 fun minDivisor(n: Int): Int {
-    var x: Int = Int.MAX_VALUE
-    for (i in 2..n) if ((n % i == 0) && (i <= x)) x = i
-    return x
+
+    for (i in 2..sqrt(n.toDouble()).toInt())
+        if (n % i == 0)
+            return i
+    return n
 }
 
 
@@ -119,11 +130,8 @@ fun minDivisor(n: Int): Int {
  *
  * Для заданного числа n > 1 найти максимальный делитель, меньший n
  */
-fun maxDivisor(n: Int): Int {
-    var x: Int = 0
-    for (i in 1 until n) if ((n % i == 0) && (i > x)) x = i
-    return x
-}
+fun maxDivisor(n: Int): Int = n / minDivisor(n)
+
 
 /**
  * Простая
@@ -133,8 +141,10 @@ fun maxDivisor(n: Int): Int {
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
 fun isCoPrime(m: Int, n: Int): Boolean {
-    var k: Boolean = true
-    for (i in 2..max(m, n)) if ((m % i == 0) && (n % i == 0)) k = false
+    var k = true
+    for (i in 2..max(m, n))
+        if ((m % i == 0) && (n % i == 0))
+            k = false
     return k
 }
 
@@ -146,8 +156,11 @@ fun isCoPrime(m: Int, n: Int): Boolean {
  * Например, для интервала 21..28 21 <= 5*5 <= 28, а для интервала 51..61 квадрата не существует.
  */
 fun squareBetweenExists(m: Int, n: Int): Boolean {
-    var k: Boolean = false
-    for (i in m..n) if ((sqrt(i.toDouble()).toInt()) * (sqrt(i.toDouble()).toInt()) == i) k = true
+    //Тут пока долго делается. До октябрьской проверки попробую придумать метод оптимизации.
+    var k = false
+    for (i in m..n)
+        if ((sqrt(i.toDouble()).toInt()) * (sqrt(i.toDouble()).toInt()) == i)
+            k = true
     return k
 }
 
@@ -168,10 +181,13 @@ fun squareBetweenExists(m: Int, n: Int): Boolean {
  * этого для какого-либо начального X > 0.
  */
 fun collatzSteps(x: Int): Int {
-    var k: Int = 0
-    var l: Int = x
+    var k = 0
+    var l = x
     while (l != 1) {
-        if (l % 2 == 0) l /= 2 else l = 3 * l + 1
+        if (l % 2 == 0)
+            l /= 2
+        else
+            l = 3 * l + 1
         k += 1
     }
     return k
@@ -189,17 +205,21 @@ fun collatzSteps(x: Int): Int {
 fun sin(x: Double, eps: Double): Double = TODO()
 
 // К сожалению, алгоритм пока не работает((( Но где я ошибся?!
-/**{
-var sinus: Double = x
-var k: Int = 3
-var n: Int = 2 * 3
-var second: Boolean = false
-while (abs(x.pow(k) / n) >= abs(eps)) {
+// upd: До сих пор не работает, хотя я напряг мозги и попытался вспомнить тригонометрию с её  2 * PI
+// и некой цикличностью sin. Нужна помощь!
+// P.S. пока мой алгоритм подразумевает, что sin при любом x будет положителен. Исправлю это, когда будут нормально
+// проходить тесты там, где sin ДОЛЖЕН быть положительным.
+/*{
+var sinus: Double = x % (2 * PI)
+var k = 3
+var n = 2 * 3
+var second = false
+while (abs((x % (2 * PI)).pow(k) / n) >= abs(eps)) {
 if (!second) {
-sinus -= x.pow(k) / n
+sinus -= abs((x % (2 * PI)).pow(k)) / n
 second = true
 } else {
-sinus += x.pow(k) / n
+sinus += abs((x % (2 * PI)).pow(k)) / n
 second = false
 }
 k += 2
@@ -207,7 +227,7 @@ n *= (k - 1) * k
 }
 return sinus
 }
- */
+*/
 
 /**
  * Средняя
@@ -228,11 +248,13 @@ fun cos(x: Double, eps: Double): Double = TODO()
  * Использовать операции со строками в этой задаче запрещается.
  */
 fun revert(n: Int): Int {
-    var l: Int = n
-    var num: Int = 0
+    var l = n
+    var num = 0
     while (l > 0) {
-
-        if (l / 10 != 0) num = (num + l % 10) * 10 else num += l % 10
+        if (l / 10 != 0)
+            num = (num + l % 10) * 10
+        else
+            num += l % 10
         l /= 10
     }
     return num
@@ -247,7 +269,20 @@ fun revert(n: Int): Int {
  *
  * Использовать операции со строками в этой задаче запрещается.
  */
-fun isPalindrome(n: Int): Boolean = TODO()
+
+
+fun isPalindrome(n: Int): Boolean {
+    var r = revert(abs(n))
+    var l = abs(n)
+    var bool = true
+    while (l > 0) {
+        if (l % 10 != r % 10)
+            bool = false
+        r /= 10
+        l /= 10
+    }
+    return bool
+}
 
 /**
  * Средняя
@@ -258,15 +293,19 @@ fun isPalindrome(n: Int): Boolean = TODO()
  * Использовать операции со строками в этой задаче запрещается.
  */
 fun hasDifferentDigits(n: Int): Boolean {
-    var l: Int = abs(n)
-    var j: Int = l % 10
-    var bool: Boolean = false
+    var l = abs(n)
+    var j = l % 10
+    var bool = false
     while (l > 9) {
-        if ((l / 10) % 10 != j) bool = true
+        if ((l / 10) % 10 != j)
+            bool = true
         l /= 10
         j = l % 10
     }
-    return if (abs(n) < 10) false else bool
+    return if (abs(n) < 10)
+        false
+    else
+        bool
 }
 
 /**
@@ -281,34 +320,34 @@ fun hasDifferentDigits(n: Int): Boolean {
 fun squareSequenceDigit(n: Int): Int = TODO()
 // Пока не готово
 /**{
-    var k: Int = 1
-    var j: Int = 1
-    var big: Long = 1
-    var num: Int = 0
-    var square: Int = 0
-    var ten: Long = 10
-    while (j != n) {
-        j += num
-        ten = 10
-        k += 1
-        square = k * k
-        num = 0
-        while (square != 0) {
-            num += 1
-            square /= 10
-        }
-        for (i in 1..num) ten *= 10
-        big = big * 10 + k * k
-        for (i in 1..num) {
-            if (j + i == n) {
-                j = n
-            }
-        }
-    }
-    big %= 10
-    return big.toInt()
+var k: Int = 1
+var j: Int = 1
+var big: Long = 1
+var num: Int = 0
+var square: Int = 0
+var ten: Long = 10
+while (j != n) {
+j += num
+ten = 10
+k += 1
+square = k * k
+num = 0
+while (square != 0) {
+num += 1
+square /= 10
 }
-*/
+for (i in 1..num) ten *= 10
+big = big * 10 + k * k
+for (i in 1..num) {
+if (j + i == n) {
+j = n
+}
+}
+}
+big %= 10
+return big.toInt()
+}
+ */
 
 
 /**
